913fc15f256ce8693f4a1af7f839b0de2445e5cc
fabjiro <fjiron2001@gmail.com>
2022-08-01 16:04:52 -0600

    fix: sharp v2

--- src/helpers/sharp.js
+++ src/helpers/sharp.js
@@ -4,9 +4,7 @@ const { readFileSync } = require("fs");
 /* Resizing the image to 864x1080 and converting it to png. */
 module.exports = async (file) => {
   await sharp(readFileSync(file.path))
-    .jpeg({
-      quality: 75,
-    })
+    .toFormat("png")
     .png({
       quality: 75,
     })
